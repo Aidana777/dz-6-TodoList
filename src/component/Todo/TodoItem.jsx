4a861af9cc95a4dcb5b4e93c0dec1d1019f59a7c
Aidana777 <aidananiazalieva27@gmail.com>
2023-04-08 18:02:51 +0600

    fix

--- src/component/Todo/TodoItem.jsx
+++ src/component/Todo/TodoItem.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import './todo.css';
+
+
+
+
 const TodoItem = ({ todos, setTodos, input, setInput, editTodo, setEditTodo }) => {
-
-
-
   const onInputChange = (event) => {
     setInput(event.target.value)
   }
